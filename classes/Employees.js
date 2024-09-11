a3e2096bb01e3b3a4aae4c8b7bc42f9cc88aff55
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-11 10:07:10 -0400

    Employee class was created and passed all the tests

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,10 +1,40 @@
 class Employees {
-
-
-
-    
-}
-
-module.exports = {
+    #salary;
+    #isHired;
+    #allEmployees=[]
+    constructor(name, position, salary) {
+      this.name = name;
+      this.position = position;
+      this.#salary = salary;
+      this.#isHired = true;
+      
+    }
+    // Getter method for salary
+    getSalary() {
+      return this.#salary;
+    }
+  
+    // Setter method to update salary
+    setSalary(newSalary) {
+      this.#salary = newSalary;
+    }
+    // Getter method for hire status
+    getStatus() {
+      return this.#isHired;
+    }
+  
+    // Method to terminate employee (change hire status)
+    setStatus(command) {
+     if(command=="fire"){
+        this.#isHired=false
+     }
+     else if (command=="hire"){
+      this.#isHired=true;
+     }
+    }
+  }
+  
+  module.exports = {
     Employees,
-}
+  };
+  

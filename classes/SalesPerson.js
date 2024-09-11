adeac2721d5cf00c331c6eaba6368405ab1cfd25
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-11 12:25:24 -0400

    SalesPerson class was added and passed all the test

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,10 +1,20 @@
-class SalesPerson {
+const { Employees } = require("./Employees");
 
-
-
-    
+class SalesPerson extends Employees {
+  #totalSales;
+  constructor(name, position, salary, client = []) {
+    super(name, position, salary);
+    this.client = client;
+    this.#totalSales = 0;
+  }
+  getSalesNumber() {
+    return this.#totalSales;
+  }
+  makeSale(amount) {
+    this.#totalSales += amount;
+  }
 }
 
 module.exports = {
-    SalesPerson,
-}
+  SalesPerson,
+};
